741bca9d9bc9ef5e07357e20b2df891e67f7b1f3
Nxjiii <Najibasm178@gmail.com>
2025-02-08 19:46:25 +0000

    App logo

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { View, Text } from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
+import LoadingScreen from './Screens/LoadingScreen';
+import ProfileScreen from './Screens/ProfileScreen';
+import ChatbotScreen from './Screens/ChatbotScreen';
+import CommunityScreen from './Screens/CommunityScreen';
+
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function HomeScreen() {
+
+
+// Bottom Tab Navigator for main screens
+function MainTabs() {
   return (
-    <View>
-      <Text>Welcome to Pure Eats!</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Chatbot" component={ChatbotScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Community" component={CommunityScreen} />
+
+    </Tab.Navigator>
   );
 }
 
+// Main App Navigation
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Loading">
+        <Stack.Screen name="Loading" component={LoadingScreen} />
+        <Stack.Screen name="Main" component={MainTabs} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Screens/ChatbotScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ChatbotScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Chatbot Screen</Text>
+    </View>
+  );
+}
+
+export default ChatbotScreen;
