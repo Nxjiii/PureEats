416c61e2b0918a486d0e532adddfdd59aeb4a9a6
Nxjiii <Najibasm178@gmail.com>
2025-04-25 04:10:35 +0100

    delete button created:). Next step is to display account details in profile.

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -4,10 +4,10 @@ import { SafeAreaView, StyleSheet, Text, TouchableOpacity, Alert, ActivityIndica
 import { useNavigation } from '@react-navigation/native';
 import { supabase } from '../lib/supabaseClient';
 
+
 function ProfileScreen() {
   const navigation = useNavigation();
-
-
+  
      //Logging out
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
@@ -18,14 +18,54 @@ function ProfileScreen() {
 
 
   //Account deletion
-  //gonna host separate function for this and call it in the button
+  //gonna host separate function for this and call this in the button
+  const handleDeleteAccount = async () => {
+    const { data: { user } } = await supabase.auth.getUser();
+  
+    try {
+      const res = await fetch(`https://delete-gt53.onrender.com/admin/delete-user/${user.id}`, {
+        method: 'DELETE',
+      });
+  
+      const result = await res.json();
+  
+      if (res.ok) {
+        await supabase.auth.signOut();
+        Alert.alert('Account deleted');
+      } else {
+        Alert.alert('Error', result.error || 'Something went wrong');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error', 'Failed to delete account');
+    }
+  };
+
+  //Alert
+  const confirmDelete = () => {
+    Alert.alert(
+      'Confirm Delete',
+      'Are you sure you want to delete your account? This cannot be undone.',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        { text: 'Delete', style: 'destructive', onPress: handleDeleteAccount },
+      ]
+    );
+  };
+  
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Profile Screen</Text>
+
       <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
         <Text style={styles.logoutText}>Logout</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity style={styles.DeleteButton} onPress={confirmDelete}>
+      <Text>Delete Account</Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 }
@@ -51,6 +91,15 @@ const styles = StyleSheet.create({
   logoutText: {
     color: '#E0E0E0',
     fontSize: 16,
+  },
+
+  DeleteButton: {
+    backgroundColor: '#D32F2F', 
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 20,
+    marginTop: 20, 
+    
   },
 });
 
